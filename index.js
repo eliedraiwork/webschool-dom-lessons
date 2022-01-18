1a5ad9d61b95ff0d50cc691f1f39f3dd68c3f385
Elie Drai <elie.drai.work@gmail.com>
2022-01-18 23:09:37 +0200

    add getters

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function objectWindow() {}
+function getters() {
+	//1. get element by id
+	const myH1Title = document.getElementById('myTitle');
 
-function objectDocument() {}
+	//2. get elements by tag name
+	const myLis = document.getElementsByTagName('li');
 
-function printDocument() {
-	console.log(document);
+	//3. query selector
+	const myTitle = document.querySelector('#myTitle');
+
+	//4. query selector all
+	const allParagraphs = document.querySelectorAll('.paragraph');
 }
